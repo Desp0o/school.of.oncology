35bf0a18b1ef5795bf7b93cd42ff1dcfe0a33232
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-08-23 16:31:55 +0400

    added folders structure / scrollToTop / routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import {Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-     
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter as Router } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <ScrollToTop />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
