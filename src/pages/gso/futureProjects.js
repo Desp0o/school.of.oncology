b4d47303bb7cd2ebf9d79656e67d17ac40fe6262
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-08-25 14:28:13 +0400

    future project image swap

--- src/pages/gso/futureProjects.js
+++ src/pages/gso/futureProjects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./futureProjects.css"
-import bla from "../../utils/images/conference1/con0.webp"
+import futureCover from "../../utils/images/futureProjectGSO.webp"
 import CardComponent from '../../components/cardComponent/CardComponent'
 
 export default function FutureProjects() {
@@ -11,7 +11,7 @@ export default function FutureProjects() {
             <p className='compl_proj_title'>სამომავლო პროექტები</p>
 
             <div className='future_cards'>
-                <CardComponent cover={bla} title="20.04.2024" link="/pages/gso/completedProjectsGSO/upcomingConference"/>
+                <CardComponent cover={futureCover} title="20.04.2024" link="/pages/gso/completedProjectsGSO/upcomingConference"/>
             </div>
         
       </div>
