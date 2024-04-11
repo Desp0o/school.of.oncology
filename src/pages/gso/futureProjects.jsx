474eaa279d7e5333a9d4257847962bc6fb3ba9b4
Despo <tornike.despotashvili@gmail.com>
2024-04-11 13:32:06 +0400

    migaret to vite and add some stuff

--- src/pages/gso/futureProjects.jsx
+++ src/pages/gso/futureProjects.jsx
@@ -6,12 +6,12 @@ import CardComponent from '../../components/cardComponent/CardComponent'
 export default function FutureProjects() {
   return (
     <>
-      <div className='future_page'>
+      <div className='future_page' style={{paddingBottom:"100px"}}>
         
             <p className='compl_proj_title'>სამომავლო პროექტები</p>
 
             <div className='future_cards'>
-                <CardComponent cover={futureCover} title="20.04.2024" link="/pages/gso/completedProjectsGSO/upcomingConference"/>
+                <CardComponent height={'fit-content'} cover={futureCover} title="20.04.2024" link="/pages/gso/completedProjectsGSO/upcomingConference"/>
             </div>
         
       </div>
